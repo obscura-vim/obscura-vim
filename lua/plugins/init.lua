92ed6bfd547dd2d7ce1b3c5bee93b198e855ae8e
alchemmist <anton.ingrish@gmail.com>
2026-02-04 13:55:28 +0300

    fix autcomplition after charecter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -104,8 +104,10 @@ local plugins = {
 				trigger = {
 					show_on_insert = false,
 					show_on_keyword = false,
+					show_on_trigger_character = false,
 				},
 				menu = {
+					auto_show = false,
 					border = "rounded",
 					winblend = 0,
 					scrollbar = false,
