28c7c7cef041b40c4d98048c28e8183f017bf29b
alchemmist <anton.ingrish@gmail.com>
2026-02-08 10:41:06 +0300

    moss theme saving state

--- init.lua
+++ init.lua
@@ -7,3 +7,12 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 require("plugins")
+
+
+vim.g.last_theme = vim.fn.stdpath("data") .. "/last_theme.txt"
+
+if vim.fn.filereadable(vim.g.last_theme) == 1 then
+    local theme = vim.fn.readfile(vim.g.last_theme)[1]
+    vim.o.background = theme
+    vim.cmd("colorscheme moss")
+end
